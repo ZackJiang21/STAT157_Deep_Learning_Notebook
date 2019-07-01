bb212a941812a71f60c3f84244f0a389120b6e2f
ZackJiang21 <zackjiang@foxmail.com>
2019-07-01 17:26:26 +0400

    Add 3.5 Image Classification Data

--- d2l/__init__.py
+++ d2l/__init__.py
@@ -1,6 +1,8 @@
 from IPython import display
 from matplotlib import pyplot as plt
 
+import sys
+
 
 def use_svg_display():
     # Use the svg format to display plot in jupyter
@@ -11,3 +13,25 @@ def set_figsize(figsize=(3.5, 2.5)):
     # Change the default figure size
     use_svg_display()
     plt.rcParams['figure.figsize'] = figsize
+
+
+def show_images(imgs, num_rows, num_cols, titles=None, scale=1.5):
+    """Plot a list of images."""
+    figsize = (num_cols * scale, num_rows * scale)
+    _, axes = plt.subplots(num_rows, num_cols, figsize=figsize)
+    axes = axes.flatten()
+    for i, (ax, img) in enumerate(zip(axes, imgs)):
+        ax.imshow(img.asnumpy())
+        ax.axes.get_xaxis().set_visible(False)
+        ax.axes.get_yaxis().set_visible(False)
+        if titles:
+            ax.set_title(titles[i])
+    return axes
+
+
+def get_dataloader_workers(num_workers=4):
+    # 0 means no additional process is used to speed up the reading of data.
+    if sys.platform.startswith('win'):
+        return 0
+    else:
+        return num_workers
